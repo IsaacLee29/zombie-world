dc83eff23a3fe7776dc639a4a985538084c10db0
Isaac Lee <ilee0014@student.monash.edu>
2020-05-05 23:03:20 +0800

    Add Limb, Arm & Leg Classes

---
+++ src/game/Arm.java
@@ -0,0 +1,14 @@
+package game;
+
+public class Arm extends Limb{
+
+    static final String ARM_DESCRIPTION = "Arm";
+
+    public Arm() {
+    }
+
+    @Override
+    public String toString() {
+        return ARM_DESCRIPTION;
+    }
+}
---
+++ src/game/Leg.java
@@ -0,0 +1,14 @@
+package game;
+
+public class Leg extends Limb {
+
+    public static final String LEG_DESCRIPTION = "Leg";
+
+    public Leg() {
+    }
+
+    @Override
+    public String toString() {
+        return LEG_DESCRIPTION;
+    }
+}
