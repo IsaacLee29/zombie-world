16fd917a21a915a3cd59ef629ca4c5f86976c6ad
Isaac Lee <ilee0014@student.monash.edu>
2020-05-16 17:29:46 +0800

    Add ZombieType interface

---
+++ src/game/ZombieType.java
@@ -0,0 +1,10 @@
+package game;
+
+public interface ZombieType {
+
+    /**
+     * Get this Type of Zombie Actor in the Zombie World.
+     * @return
+     */
+    Enum<?> getTypeOfZombieActor();
+}
